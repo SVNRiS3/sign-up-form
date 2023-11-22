d1f00fe2e866bd5533b86d9dd969514bfe21cff4
SVNRiS3 <krzy.driwa@gmail.com>
2023-11-22 12:35:14 +0100

    Add password confirmation verification

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello!");
+const pwds = document.querySelectorAll(".error");
+const submitButton = document.querySelector(".submit-button");
+
+pwds.forEach((pwd) => {
+	pwd.addEventListener("input", (e) => {
+		pwds[0].children[1].value === pwds[1].children[1].value &&
+		pwds[0].children[1].value.length > 0
+			? pwds.forEach((pwd) => {
+					pwd.classList.remove("error");
+			  })
+			: pwds.forEach((pwd) => {
+					pwd.classList.add("error");
+			  });
+	});
+});
+
+submitButton.addEventListener("click", (e) => {
+	if (pwds[0].classList.contains("error")) {
+		e.preventDefault();
+		alert("Passwords do not match!");
+	}
+});
